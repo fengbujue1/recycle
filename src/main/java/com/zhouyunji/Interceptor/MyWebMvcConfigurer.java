9125312278a81875e959b601fb4c3e813fd7064d
fengbujue <619577235@qq.com>
2021-12-31 13:53:24 +0800

    2021.12.31 1.改了下包名字，清晰一点

--- src/main/java/com/zhouyunji/Interceptor/MyWebMvcConfigurer.java
+++ src/main/java/com/zhouyunji/Interceptor/MyWebMvcConfigurer.java
@@ -1,8 +1,7 @@
-package com.zhouyunji.login;
+package com.zhouyunji.Interceptor;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
